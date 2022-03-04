7b3b64efb46cc3c455c05a951613d607e1412b7b
dikewarda <dikeayuw@gmail.com>
2022-03-04 21:00:31 +0700

    tugas3

---
+++ README.md
@@ -0,0 +1,11 @@
+# Jobsheet 3
+
+Nama: Dike Ayu Wardani <br/>
+NIM: 2031710147 <br/>
+Kelas: MI-2C <br/>
+Prodi: D3 MI <br/>
+
+## Hasil Praktikum Jobsheet 3
+![Screenshot Hasil](img/1_hasil.jpg)
+![Screenshot Hasil](img/2_hasil.jpg)
+![Screenshot Hasil](img/3_hasil.jpg)
---
+++ lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  double _inputUser = 0;
+  double _kelvin = 0;
+  double _reamur = 0;
+
+  final temperaturController = TextEditingController();
+
+  @override
+  void dispose() {
+    temperaturController.dispose();
+    super.dispose();
+  }
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Konversi Suhu - 2031710147 Dike Ayu Wardani',
+        theme: ThemeData(
+          primarySwatch: Colors.pink,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Konversi Suhu - 2031710147 Dike Ayu Wardani"),
+          ),
+          body: Container(
+            margin: const EdgeInsets.all(8),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                TextFormField(
+                  controller: temperaturController,
+                  decoration: const InputDecoration(
+                      labelText: 'Masukkan Suhu Dalam Celcius'),
+                  keyboardType:
+                      const TextInputType.numberWithOptions(decimal: true),
+                  inputFormatters: [
+                    FilteringTextInputFormatter.allow(RegExp(r'[0-9.]'))
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        const Text("Suhu Dalam Kelvin"),
+                        Text(
+                          _kelvin.toStringAsFixed(2),
+                          style: const TextStyle(fontSize: 32),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        const Text("Suhu Dalam Reamur"),
+                        Text(
+                          _reamur.toStringAsFixed(2),
+                          style: const TextStyle(fontSize: 32),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                MaterialButton(
+                  onPressed: () {
+                    convert();
+                  },
+                  child: const Text(
+                    "Konversi",
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  color: Colors.pink,
+                  textColor: Colors.white,
+                  minWidth: double.maxFinite,
+                  padding: const EdgeInsets.all(8),
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+
+  void convert() {
+    _inputUser = double.parse(temperaturController.text);
+    setState(() {
+      _kelvin = _inputUser + 273.15;
+      _reamur = _inputUser * 0.8;
+    });
+  }
+}
